b4a34a5dbcb95c334f79ae3dde89cffb37b6e1f1
Daniel Eyster <danieleyster7@gmail.com>
2015-08-19 22:02:01 -0400

    -Added comments -Cleaned up code

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
-//use PB4 for passenger (right) DIR connection (protoboard B-62)
-//use PB5 for driver (left) DIR connection  (protoboard B-61)
-//use PA1 for passenger (right) sensor connection (protoboard A-48)
-//use PA0 for driver (left) sensor connection  (protoboard A-47)
-//use PP1 for passenger (right) EN connection  (protoboard A-39)
-//use PP3 for driver (left) EN connection (protoboard A-37)
-//use PJ7 for lap counter interrupt (protoboard B-46)
+/*
+	ECET365 Final Project - Daniel Eyster
+	PB4 for passenger (right) DIR connection (protoboard B-62)
+	PB5 for driver (left) DIR connection  (protoboard B-61)
+	PA1 for passenger (right) sensor connection (protoboard A-48)
+	PA0 for driver (left) sensor connection  (protoboard A-47)
+	PP1 for passenger (right) EN connection  (protoboard A-39)
+	PP3 for driver (left) EN connection (protoboard A-37)
+	PJ7 for lap counter interrupt (protoboard B-46)
+*/
 
 #include <hidef.h>           /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
@@ -16,8 +19,8 @@ void init_Ports(void);
 void init_PJ7(void);
 
 //global variables
-unsigned int count = 0;
-unsigned int counter = 0;
+unsigned int lapCount = 0;
+unsigned int timeCount = 0;
 unsigned char adjustMotor = 0;
 
 //#define Driver 0x01
@@ -30,9 +33,9 @@ unsigned char adjustMotor = 0;
 #define Passenger_DIR 0x10
 
 const struct State {
-  unsigned char PWMduty3;         //output data to PMWDTY for driver speed
-  unsigned char PWMduty1;         //output data to PWMDTY for passenger speed 
-  const struct State *Next[8];    //next state if input = 0,1,etc.
+	unsigned char PWMduty3;         //output data to PMWDTY for driver speed
+	unsigned char PWMduty1;         //output data to PWMDTY for passenger speed 
+	const struct State *Next[8];    //next state if input = 0,1,etc.
 };
 
 #define STRAIGHT &fsm[0]
@@ -68,65 +71,76 @@ StateType fsm[6] = {
 
 void main(void){
   
-  StateType *Pt;                     //pointer to present state
-  unsigned char sensor;             //sensor for the steering inputs
-      
-  Pt = STRAIGHT;                          //initialize the present state
-  
-  init_PWM( );
-  init_Ports( );
-  init_PJ7( );
-   
-  EnableInterrupts;
-  
-  for(;;) 
-  {
-    if(adjustMotor == 1) { 
-    
-      sensor = PORTA & 0x05;              
-      
-      Pt = Pt->Next[sensor];              //move to next state
-
-      PWMDTY3 = Pt->PWMduty3 ;            //set duty cycle for driver motor
-      PWMDTY1 = Pt->PWMduty1 ;          //set duty cycle for passenger motor
-      adjustMotor == 0;
-          
-    }else PWME = 0x00;
-   
-  } 
+	StateType *Pt;       
+	unsigned char sensor;
+	
+	//Set pointer at STRAIGHT
+	Pt = STRAIGHT;                      
+	
+	//Initialize PWM and Ports
+	init_PWM( );
+	init_Ports( );
+	init_PJ7( );
+	   
+	EnableInterrupts;
+	
+	for(;;) 
+	{	 
+		//Only run for 2 laps
+		if(lapCount <= 2)
+		{
+			//Adjust motor every time interval
+			if(adjustMotor == 1) 
+			{    
+				//Check PT0-PT2 for sensors
+				sensor = PORTA & 0x05;              
+				//Move to next state via sensor input 
+				Pt = Pt->Next[sensor];           
+				//Set motor output based on state
+				PWMDTY3 = Pt->PWMduty3 ;          
+				PWMDTY1 = Pt->PWMduty1 ;     
+				//Reset adjust flag
+				adjustMotor == 0;				  
+			}
+		}
+		else {
+			//Turn off motors
+			PWME = 0x00;
+		}
+	} 
 }
 
 
 /******* functions ********/
 void init_PWM()
 {
-  PWMCAE = 0x00;                // left aligned
-  PWMPOL = 0x0A;                // initial HIGH output on ch. 1 and 3 
-  PWMCLK = 0x00;                // Ch 1 source is clock A, ch 3 use clock B 
-  PWMCLKAB = 0x00;              // Use clock A and B respectively
-  PWMPRCLK = 0x55;              // Clk A/B pre-scale = 32 (PWM clock = 6.25MHz/32 = 195.3125 KHz =>period = 5.12uS)
-  PWMPER1 = 0xC8;                // obtain a period width about 1.024ms to create 976.56Hz PWM
-  PWMPER3 = 0xC8;                // obtain a period width about 1.024ms to create 976.56Hz PWM
-  PWMDTY1 = 0xAA;               // 85% duty cycle
-  PWMDTY3 = 0xAA;               // 85% duty cycle
-  PWME = 0x0A ;                 // enable channel 1 and channel 3
+	PWMCAE = 0x00;                // left aligned
+	PWMPOL = 0x0A;                // initial HIGH output on ch. 1 and 3 
+	PWMCLK = 0x00;                // Ch 1 source is clock A, ch 3 use clock B 
+	PWMCLKAB = 0x00;              // Use clock A and B respectively
+	PWMPRCLK = 0x55;              // Clk A/B pre-scale = 32 (PWM clock = 6.25MHz/32 = 195.3125 KHz =>period = 5.12uS)
+	PWMPER1 = 0xC8;                // obtain a period width about 1.024ms to create 976.56Hz PWM
+	PWMPER3 = 0xC8;                // obtain a period width about 1.024ms to create 976.56Hz PWM
+	PWMDTY1 = 0xAA;               // 85% duty cycle
+	PWMDTY3 = 0xAA;               // 85% duty cycle
+	PWME = 0x0A ;                 // enable channel 1 and channel 3
 }
 
 void init_Ports( )
 { 
-  DDRB |= 0x30;                 //let PB3 and PB5 be outputs
-  PORTB |= Driver_DIR;          //set driver motor direction
-  PORTB &= ~Passenger_DIR;      //set passenger motor direction
-  DDRA = 0x00;                  //PORTA as inputs
+	DDRB |= 0x30;                 //let PB3 and PB5 be outputs
+	PORTB |= Driver_DIR;          //set driver motor direction
+	PORTB &= ~Passenger_DIR;      //set passenger motor direction
+	DDRA = 0x00;                  //PORTA as inputs
 }
 
 void init_PJ7()
 {
-  DDRJ = 0x00;                  //PORT J7 interrupt input
-  PPSJ = 0x00;                  //pull-up PortJ; define falling edge trigger
-  PERJ = 0xFF;                  //enable pulls
-  PIFJ = 0x80;                  //clear the PORT J7 interrupt flag before enabling
-  PIEJ = 0x80;                  //PORT J7 = 1; enable interrupt
+	DDRJ = 0x00;                  //PORT J7 interrupt input
+	PPSJ = 0x00;                  //pull-up PortJ; define falling edge trigger
+	PERJ = 0xFF;                  //enable pulls
+	PIFJ = 0x80;                  //clear the PORT J7 interrupt flag before enabling
+	PIEJ = 0x80;                  //PORT J7 = 1; enable interrupt
 }
 
 void init_clock()
@@ -140,16 +154,15 @@ void init_clock()
 #pragma CODE_SEG NON_BANKED 
 interrupt ( ( (0x10000 - 0xFFF0) / 2 ) - 1 )  void isrRTI(void)
 {
-
-    counter++;
-	if (counter == 1000) {
+    timeCount++;
+	//If ____s passed, set ajust flag and reset timer
+	if (timeCount == 1000) {
 		adjustMotor = 1;
-		counter = 0;
+		timeCount = 0;
 	}
-  //Clear flag
-  
- CPMUFLG_RTIF = 1;          // clear RTIF
-  return;
+	//Clear flag
+	CPMUFLG_RTIF = 1;          // clear RTIF
+	return;
 }
 
 #pragma CODE_SEG DEFAULT  
@@ -159,8 +172,8 @@ interrupt ( ( (0x10000 - 0xFFF0) / 2 ) - 1 )  void isrRTI(void)
 #pragma CODE_SEG NON_BANKED
 interrupt ( ( (0x10000 - 0xFFCE) / 2) - 1) void INTERRUPT_IRQISR(void)
 {
-  count++;
-  PIFJ = 0x80;                  //clear interrupt flag for PORT J7
+	lapCount++;
+	PIFJ = 0x80;                  //clear interrupt flag for PORT J7
 }
 
 /************** End of Program File *****************/
